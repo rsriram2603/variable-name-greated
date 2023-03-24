4a24357a3ffac7f05cf5f8adbfe070c8609fdc66
rsriram2603 <rsriram830@gmail.com>
2023-03-24 11:14:15 +0530

    new file add

---
+++ main.js
@@ -0,0 +1,22 @@
+var anyFunctionForAnyone=true;
+	var bankWrok=true;
+	var busAvailability=false;
+	var bodyTemp=99;
+	var goingToTown=false;
+	var moreHomeWork=false;
+	var wakeupTime=8;
+	var isManasuGood=true;
+	var bikeWorking=false;
+	var localMatch=true;
+	var templeFestival=false; 
+	var isRaining=false;
+	var worthTopic="good";
+	var movieRelease=true;
+	if(anyFunctionForAnyone==true || bankWrok==true || busAvailability=="no" || bodyTemp>=98 || goingToTown==true || moreHomeWork==true || wakeupTime>9 || isManasuGood!=true || bikeWorking!=true || localMatch==true || templeFestival==true || isRaining== true || worthTopic!="good" || movieRelease==true)
+	{
+		console.log("take leave");
+	}	
+	else
+	{
+		console.log("come to fabevy");
+	}
